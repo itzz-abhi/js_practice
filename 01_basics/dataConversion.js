6c9e69ef286ad9905f6ff608fffa9185bc32cd09
itzz-abhi <itsabhi8433@gmail.com>
2025-05-26 19:11:03 +0000

    config done

---
+++ 01_basics/dataConversion.js
@@ -0,0 +1,23 @@
+// let score ="33";
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let num ="33abc";
+// console.log(typeof num);
+// let valueInNumber = Number(num);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isEqual = true;
+// console.log(typeof isEqual);
+// let valueInNumber = Number(isEqual);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let accountName;
+console.log(accountName);
+valueInString = String(accountName);
+console.log(typeof valueInString);
+console.log(valueInString);
