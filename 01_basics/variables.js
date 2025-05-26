30e2d152dfddcdfd3afbc93d8b5c3d0069ac81fb
itzz-abhi <itsabhi8433@gmail.com>
2025-05-26 18:45:58 +0000

    config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,27 @@
+// const accountId = 12345;
+// let accountName ="Abhishek Singh";
+// let accountPassword ="8433";
+// console.log(accountPassword);
+// {
+//     let accountPassword ="8126";
+//     console.log(accountPassword);
+// }
+// console.log(accountPassword);
+// var accountState ="Uttar Pradesh";
+// console.log(accountState);
+// {
+//     var accountState ="Madhya Pradesh";
+//     console.log(accountState);
+// }
+// console.log(accountState);
+
+// Note:- var:-Global Scope and let/const:- local Scope
+
+let accountId = 12345;
+let accountName ="Abhishek Singh";
+let accountPassword ="8433";
+let accountState ="Uttar Pradesh";
+let accountCity ="Mathura";
+let accountBalance;
+// console.table([accountId,accountName,accountPassword,accountState,accountCity])
+console.log(accountId+","+accountName+","+accountPassword+","+accountState+","+accountCity+","+accountBalance);
